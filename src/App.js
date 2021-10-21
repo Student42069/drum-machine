a3c6f036f1642b93d5d234e63794d16ac15458c3
Student42069 <lehackdeshackers@gmail.com>
2021-10-21 00:00:24 -0400

    basic drum pad

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
 
+const drums = ["q", "w", "e", "a", "s", "d", "z", "x", "c"];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>An app</p>
-        <Text />
-      </header>
+    <div className="App" id="drum-machine">
+      <h1>Drum Machine</h1>
+      <div id="display">
+        {drums.map((letter) => {
+          return <Drum text={letter.toUpperCase()} key={letter} />;
+        })}
+      </div>
     </div>
   );
 }
 
-function Text() {
-  return <h1>Text test</h1>;
+function Drum(p) {
+  return (
+    <div className="drum-pad" id={p.text.toLowerCase()}>
+      <h2>{p.text}</h2>
+    </div>
+  );
 }
 
 export default App;
