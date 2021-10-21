f2808b21a03a653a914793fbf6df926ca666c8c0
Student42069 <lehackdeshackers@gmail.com>
2021-10-21 00:42:00 -0400

    sound of one board incorporated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import "./App.css";
-
-const drums = ["q", "w", "e", "a", "s", "d", "z", "x", "c"];
+import bankOne from "./bankone";
 
 function App() {
   return (
     <div className="App" id="drum-machine">
       <h1>Drum Machine</h1>
       <div id="display">
-        {drums.map((letter) => {
-          return <Drum text={letter.toUpperCase()} key={letter} />;
+        {bankOne.map((letter) => {
+          return <Drum drum={letter} key={letter.id} />;
         })}
       </div>
     </div>
   );
 }
 
-function Drum(p) {
+function Drum(props) {
+  let audio = new Audio(props.drum.url);
   return (
-    <div className="drum-pad" id={p.text.toLowerCase()}>
-      <h2>{p.text}</h2>
+    <div
+      className="drum-pad"
+      id={props.drum.id}
+      onClick={() => {
+        audio.pause();
+        audio.currentTime = 0;
+        audio.play();
+      }}
+    >
+      <h2>{props.drum.keyTrigger}</h2>
+      <audio
+        src={props.drum.url}
+        className="clip"
+        id={props.drum.keyTrigger}
+      ></audio>
     </div>
   );
 }
