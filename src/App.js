2073839635ca2b5ae8fb4eb4fb9dd6c67ff8bda1
Student42069 <lehackdeshackers@gmail.com>
2021-10-22 14:08:06 -0400

    added options
    Added a switch trigger to choose a sound-board, on-off switch and a responsive text displaying the note played

--- src/App.js
+++ src/App.js
@@ -1,36 +1,77 @@
 import "./App.css";
 import bankOne from "./bankone";
+import bankTwo from "./banktwo";
+import "./slider.css";
+import React from "react";
 
 function App() {
+  const [onOff, setOnOff] = React.useState(false);
+  const [bank, setBank] = React.useState(bankOne);
+  const [name, setName] = React.useState("Drum");
+
+  const handleOn = ({ target }) => {
+    setOnOff(target.checked);
+  };
+
+  const handleBank = ({ target }) => {
+    if (target.checked) {
+      setBank(bankTwo);
+    } else {
+      setBank(bankOne);
+    }
+  };
+
   return (
     <div className="App" id="drum-machine">
       <h1>Drum Machine</h1>
-      <div id="display">
-        {bankOne.map((letter) => {
-          return <Drum drum={letter} key={letter.id} />;
-        })}
+
+      <div id="board">
+        <div id="display1">
+          {bank.map((letter) => {
+            let props = {
+              letter,
+              onOff,
+              setName,
+            };
+            return <Drum {...props} key={letter.id} />;
+          })}
+        </div>
+        <div id="commands">
+          <label className="switch">
+            <input type="checkbox" onChange={handleOn} />
+            <span className="slider"></span>
+          </label>
+          <h3 id="display">{name}</h3>
+          <label className="switch">
+            <input type="checkbox" onChange={handleBank} />
+            <span className="slider round"></span>
+          </label>
+        </div>
       </div>
     </div>
   );
 }
 
 function Drum(props) {
-  let audio = new Audio(props.drum.url);
+  let audio = new Audio(props.letter.url);
   return (
     <div
       className="drum-pad"
-      id={props.drum.id}
+      id={props.letter.id}
       onClick={() => {
-        audio.pause();
-        audio.currentTime = 0;
-        audio.play();
+        if (props.onOff) {
+          audio.pause();
+          audio.currentTime = 0;
+          audio.play();
+          props.setName(props.letter.id);
+        }
       }}
     >
-      <h2>{props.drum.keyTrigger}</h2>
+      <h2>{props.letter.keyTrigger}</h2>
       <audio
-        src={props.drum.url}
+        src={props.letter.url}
         className="clip"
-        id={props.drum.keyTrigger}
+        id={props.letter.keyTrigger}
       ></audio>
     </div>
   );
